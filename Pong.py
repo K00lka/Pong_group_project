fce596af2a15de260be718f7b5e47fb3a9304bc4
K00lka <69790586+K00lka@users.noreply.github.com>
2025-03-30 12:00:25 +0200

    Add files via upload
    First stage

---
+++ Pong.py
@@ -0,0 +1,7 @@
+#Importuje framework Pygame:
+import pygame
+#skraca pygame.locals do pygame (można by np dodać jeszcze "as pl", 
+# ale "l" za bardzo myli mi się z "I" i "1" xD):
+from pygame.locals import *
+
+pygame.init()
